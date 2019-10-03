b5f291a400794d080445f74334128d30ff8a9d82
Jhersy <jhersyvb@gmail.com>
2019-10-03 12:19:11 -0500

    4 Add a bill category

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 import AddCategory from './components/AddCategory'
@@ -8,17 +8,31 @@ import Chart from './components/Chart'
 import BillsTable from './components/BillsTable'
 
 function App() {
+  const [shouldShowAddCategory, setShouldShowAddCategory] = useState(true)
+  const [categories, setCategories] = useState([])
+
+  const addCategory = category => {
+    setCategories([...categories, category])
+    setShouldShowAddCategory(false)
+  }
+
   return (
     <div className="App">
-      <NavBar />
-      <div className="container flex">
-        <div className="w-1/2">
-          <BillsTable />
+      {shouldShowAddCategory ? (
+        <AddCategory onSubmit={addCategory} />
+      ) : (
+        <div>
+          <NavBar />
+          <div className="container flex">
+            <div className="w-1/2">
+              <BillsTable />
+            </div>
+            <div className="w-1/2">
+              <Chart />
+            </div>
+          </div>
         </div>
-        <div className="w-1/2">
-          <Chart />
-        </div>
-      </div>
+      )}
     </div>
   )
 }
