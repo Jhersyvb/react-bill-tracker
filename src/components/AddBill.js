8530ec138a9e6904d1cd77cce2a95a9e3c5bdfef
Jhersy <jhersyvb@gmail.com>
2019-10-04 20:11:04 -0500

    Go back to the main panel when you open a modal (add bill or add category)

--- src/components/AddBill.js
+++ src/components/AddBill.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
+import { withRouter } from 'react-router-dom'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
-export default props => {
+const AddBill = props => {
   const [amount, setAmount] = useState(0)
   const [category, setCategory] = useState(props.categories[0])
   const [date, setDate] = useState(new Date())
@@ -29,43 +30,55 @@ export default props => {
     setDate(date)
   }
 
-  const inputStyle = 'shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+  const inputStyle =
+    'shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+
+  const goBack = () => {
+    props.history.goBack()
+  }
 
   return (
-    <form className="h-100 w-full flex items-center justify-center font-sans">
-      <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
-        <div className="mb-4">
-          <h1 className="text-2xl font-semibold mb-2 text-gray-900">Enter a new bill</h1>
-          <p>E.g. 'Electricity' or 'Gas' or 'Internet'</p>
-          <div className="flex mt-4">
-            <DatePicker selected={date} onChange={handleChangeDate} className={inputStyle} />
-            <select onChange={handleChangeCategory}>
-              {props.categories
-                ? props.categories.map((value, index) => {
-                    return (
-                      <option key={index} value={value}>
-                        {value}
-                      </option>
-                    )
-                  })
-                : ''}
-            </select>
-            <input
-              className={inputStyle}
-              type="text"
-              placeholder="Write a amount"
-              value={amount}
-              onChange={handleChangeAmount}
-            />
-            <button
-              className="flex-no-shrink p-2 ml-4 rounded bg-teal-500 text-white hover:bg-teal-600"
-              onClick={handleSubmit}
-            >
-              Add
-            </button>
+    <div>
+      <form className="h-100 w-full flex items-center justify-center font-sans">
+        <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+          <div className="mb-4">
+            <h1 className="text-2xl font-semibold mb-2 text-gray-900">Enter a new bill</h1>
+            <p>E.g. 'Electricity' or 'Gas' or 'Internet'</p>
+            <div className="flex mt-4">
+              <DatePicker selected={date} onChange={handleChangeDate} className={inputStyle} />
+              <select onChange={handleChangeCategory}>
+                {props.categories
+                  ? props.categories.map((value, index) => {
+                      return (
+                        <option key={index} value={value}>
+                          {value}
+                        </option>
+                      )
+                    })
+                  : ''}
+              </select>
+              <input
+                className={inputStyle}
+                type="text"
+                placeholder="Write a amount"
+                value={amount}
+                onChange={handleChangeAmount}
+              />
+              <button
+                className="flex-no-shrink p-2 ml-4 rounded bg-teal-500 text-white hover:bg-teal-600"
+                onClick={handleSubmit}
+              >
+                Add
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+      <button className="underline" onClick={goBack}>
+        Go back
+      </button>
+    </div>
   )
 }
+
+export default withRouter(AddBill)
--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
+import { withRouter } from 'react-router-dom'
 
-export default props => {
+const AddCategory = props => {
   const [category, setCategory] = useState('')
 
   const handleCategory = e => {
@@ -17,29 +18,40 @@ export default props => {
     props.onSubmit(category)
   }
 
+  const goBack = () => {
+    props.history.goBack()
+  }
+
   return (
-    <form className="h-100 w-full flex items-center justify-center font-sans">
-      <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
-        <div className="mb-4">
-          <h1 className="text-2xl font-semibold mb-2 text-gray-900">Enter a category of bills</h1>
-          <p>E.g. 'Electricity' or 'Gas' or 'Internet'</p>
-          <div className="flex mt-4">
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="text"
-              placeholder="Add category"
-              value={category}
-              onChange={handleCategory}
-            />
-            <button
-              className="flex-no-shrink p-2 ml-4 rounded bg-teal-500 text-white hover:bg-teal-600"
-              onClick={handleSubmit}
-            >
-              Add
-            </button>
+    <div>
+      <form className="h-100 w-full flex items-center justify-center font-sans">
+        <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+          <div className="mb-4">
+            <h1 className="text-2xl font-semibold mb-2 text-gray-900">Enter a category of bills</h1>
+            <p>E.g. 'Electricity' or 'Gas' or 'Internet'</p>
+            <div className="flex mt-4">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                placeholder="Add category"
+                value={category}
+                onChange={handleCategory}
+              />
+              <button
+                className="flex-no-shrink p-2 ml-4 rounded bg-teal-500 text-white hover:bg-teal-600"
+                onClick={handleSubmit}
+              >
+                Add
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+      <button className="underline" onClick={goBack}>
+        Go Back
+      </button>
+    </div>
   )
 }
+
+export default withRouter(AddCategory)
